b48e21b859d4832708996653c7d95629f6a3cedb
AndreXC <andreluismartins2020@outlook.com>
2025-09-08 00:45:44 -0300

    modificações design

--- resources/static/resources/js/includes/theme.js
+++ resources/static/resources/js/includes/theme.js
@@ -1,48 +1,44 @@
+document.addEventListener('DOMContentLoaded', () => {
 // Seletores de elementos
 const sideMenu = document.querySelector('aside');
 const menuBtn = document.getElementById('menu-btn');
-const themeToggler = document.querySelector('.theme-toggler');
+const themeToggle = document.getElementById('menu-btn-darkmode'); // O id do seu input foi alterado aqui
 
 // Função para aplicar o modo escuro
 const applyDarkMode = () => {
-    document.body.classList.add('dark-theme-variables');
-    themeToggler.querySelector('span:nth-child(1)').classList.remove('active');
-    themeToggler.querySelector('span:nth-child(2)').classList.add('active');
+  document.body.classList.add('dark-theme-variables');
 };
 
 // Função para aplicar o modo claro
 const applyLightMode = () => {
-    document.body.classList.remove('dark-theme-variables');
-    themeToggler.querySelector('span:nth-child(1)').classList.add('active');
-    themeToggler.querySelector('span:nth-child(2)').classList.remove('active');
+  document.body.classList.remove('dark-theme-variables');
 };
 
 // Checa o localStorage ao carregar a página
-document.addEventListener('DOMContentLoaded', () => {
-    const isDarkMode = localStorage.getItem('isDarkMode');
-    if (isDarkMode === 'true') {
-        applyDarkMode();
-    } else {
-        applyLightMode();
-    }
-});
+
+  const isDarkMode = localStorage.getItem('isDarkMode');
+  if (isDarkMode === 'true') {
+    applyDarkMode();
+    themeToggle.checked = false; // Desmarcado para o modo escuro
+  } else {
+    applyLightMode();
+    themeToggle.checked = true; // Marcado para o modo claro
+  }
 
 // Evento para abrir o menu
 menuBtn.addEventListener('click', () => {
-    sideMenu.style.display = 'block';
+  sideMenu.style.display = 'block';
 });
 
-// Evento para alternar o tema
-themeToggler.addEventListener('click', () => {
-    // Alterna a classe no body
-    document.body.classList.toggle('dark-theme-variables');
-
-    // Salva o estado no localStorage
-    if (document.body.classList.contains('dark-theme-variables')) {
-        localStorage.setItem('isDarkMode', 'true');
-        applyDarkMode();
-    } else {
-        localStorage.setItem('isDarkMode', 'false');
-        applyLightMode();
-    }
+// Evento para alternar o tema com o novo checkbox
+themeToggle.addEventListener('change', () => {
+  // Salva o estado no localStorage
+  if (themeToggle.checked) {
+    localStorage.setItem('isDarkMode', 'false'); // Se marcado, é modo claro
+    applyLightMode();
+  } else {
+    localStorage.setItem('isDarkMode', 'true'); // Se desmarcado, é modo escuro
+    applyDarkMode();
+  }
+});
 });
